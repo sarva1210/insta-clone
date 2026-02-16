3fe39a1792abcc423d78ce0c9fbb26367815138f
sarva1210 <sarva.vish77@gmail.com>
2026-02-16 22:00:30 +0530

    post routes

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -3,15 +3,15 @@ const postController = require('../controllers/post.controller')
 const postRouter = express.Router()
 const multer = require('multer')
 const upload = multer({storage:multer.memoryStorage()})
-
+const identifyuser = require('../middlewares/auth.middleware')
 // post /api/posts {protected}
-postRouter.post("/",upload.single("image"), postController.createPostController)
+postRouter.post("/",upload.single("image"),identifyuser, postController.createPostController)
 
 // get /api/posts
-postRouter.get("/",postController.getPostController)
+postRouter.get("/",identifyuser,postController.getPostController)
 
 // get /api/posts/details/:postid
-postRouter.get("/details:postId", postController.getPostDetailsController)
+postRouter.get("/details:postId",identifyuser, postController.getPostDetailsController)
 
 
 module.exports = postRouter
