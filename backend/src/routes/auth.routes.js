f88a8dd11d181fbff84ed9d347819962171ba09e
sarva1210 <sarva.vish77@gmail.com>
2026-02-20 22:35:14 +0530

    routes js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const authController = require('../controllers/auth.controller')
-
+const {identifyUser} = require('../middlewares/auth.middleware')
 const authRouter = express.Router()
 
 // post: /api/auth/register
@@ -8,5 +8,9 @@ authRouter.post('/register', authController.registerController)
 
 // post: /api/auth/login
 authRouter.post('/login', authController.loginController)
+
+// get: /api/auth/get-me  -> get the current logged in users information
+//private
+authRouter.get('/get-me',{identifyUser}, authController.getMeController)
 
 module.exports = authRouter
