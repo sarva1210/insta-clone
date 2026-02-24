df7c3874a4608db9489dfe967274c5884da7ef18
sarva1210 <sarva.vish77@gmail.com>
2026-02-24 12:20:03 +0530

    initialize

---
+++ frontend/src/features/posts/hook/usePost.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { getFeed } from "../services/post.api";
+import { PostContext } from "../post.context";
+
+export const usePost=()=>{
+    const context = useContext(PostContext)
+    const { loading, setLoading, post, setPost, feed, setFeed } = context
+    const handleGetFeed = async()=>{
+        setLoading(true)
+        const data = await getFeed()
+        setFeed(data.posts)
+        setLoading(false)
+    }
+    return{loading,feed,post,handleGetFeed}
+}
