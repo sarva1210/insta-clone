90e7b0096cd5a2a5ca7d120ce4a91dbdf6b433ad
sarva1210 <sarva.vish77@gmail.com>
2026-02-24 12:20:26 +0530

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { RouterProvider } from "react-router"
 import { router } from "./AppRoutes"
 import { AuthProvider } from "./features/auth/auth.context"
 import "./features/shared/global.scss"
+import { PostContextProvider } from "./features/posts/post.context"
 
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <PostContextProvider>
+        <RouterProvider router={router} />
+      </PostContextProvider>
+
     </AuthProvider>
   )
 }
