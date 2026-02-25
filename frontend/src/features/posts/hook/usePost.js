72229d9111b59dbfbbefe47c1e48030487de084a
sarva1210 <sarva.vish77@gmail.com>
2026-02-25 13:15:29 +0530

    posts

--- frontend/src/features/posts/hook/usePost.js
+++ frontend/src/features/posts/hook/usePost.js
@@ -1,15 +1,41 @@
-import { useContext } from "react";
-import { getFeed } from "../services/post.api";
+import { useContext, useEffect } from "react";
+import { getFeed, createPost, likePost, unLikePost} from "../services/post.api";
 import { PostContext } from "../post.context";
 
 export const usePost=()=>{
     const context = useContext(PostContext)
     const { loading, setLoading, post, setPost, feed, setFeed } = context
+
     const handleGetFeed = async()=>{
         setLoading(true)
         const data = await getFeed()
-        setFeed(data.posts)
+        setFeed(data.posts.reverse())
         setLoading(false)
     }
-    return{loading,feed,post,handleGetFeed}
+
+    const handleCreatePost = async(imageFile, caption)=>{
+        setLoading(true)
+        const data = await createPost(imageFile, caption)
+        setFeed([data.post, ...feed])
+        setLoading(false)
+    }
+
+    const handleLike = async (post) => {
+
+        const data = await likePost(post)
+        await handleGetFeed()
+
+    }
+    const handleUnLike = async (post) => {
+
+        const data = await unLikePost(post)
+        await handleGetFeed()
+
+    }
+
+    useEffect(()=>{
+        handleGetFeed()
+    },[])
+
+    return{loading, feed, post, handleGetFeed, handleCreatePost, handleLike, handleUnLike}
 }
